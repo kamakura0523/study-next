c7f0cd132a8da45e88c9f0c48556c159723ee4ff
kamakura0523 <suieikama0523@gmail.com>
2024-06-12 23:08:36 +0900

    useState使ってみた

--- src/app/page1/page.jsx
+++ src/app/page1/page.jsx
@@ -1,19 +1,35 @@
-import { useDebugValue } from "react";
-import Link from "next/link";
+"use client";
+
+import React from "react";
+import { TextField } from "@mui/material";
 import styles from "../page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+  const init = 0;
+
+  const [count, setCount] = useState(init);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
-    <div className={styles.main}>
-      <div style={{ color: "red" }}>
-        <h2 className={styles.title}>商品一覧</h2>
-        <ul>
-          <li>
-            <Link href="../products">スマートフォン</Link>
-            <p>にょにょ</p>
-          </li>
-        </ul>
+    <body style={{ backgroundColor: "#d6ccc2" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "70vh",
+        }}
+      >
+        <p>aaa</p>
+        <h1>bbb</h1>
+        <TextField label="text" />
+        <button onClick={handleClick}>カウント</button>
+        <p>{count}回、クリックされました。</p>
       </div>
-    </div>
+    </body>
   );
 }
--- src/app/products/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "../page.module.css";
-
-function ProductsPage() {
-  // ページの内容をここに書く
-  return <div className="text-blue-500">Products Page</div>;
-}
-
-export default ProductsPage;
